818da0340d83b6e5dced01de5cea97bcbac1efd6
ulianaami <ulianasupergirla@gmail.com>
2022-02-27 17:43:54 +0500

    #1 added a callback_worker

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 bot = telebot.TeleBot(TOKEN)
 
-file = open('affirmations.txt', 'r')
+file = open('affirmations.txt', 'r', encoding='UTF-8')
 affirmations = file.read().split('\n')
 file.close()
 
@@ -14,7 +14,7 @@ file.close()
 def get_text_messages(message):
     username = message.from_user.username
     if message.text == 'Привет' or message.text == 'привет':
-        bot.send_message(message.from_user.id, f'Привет, {username}, чтобы получить позитивную аффирмацию, напиши: "Аффирмация"')
+        bot.send_message(message.from_user.id, f'Привет, {username}\nНапиши: "Аффирмация"')
         bot.register_next_step_handler(message, give_affirmation)
     elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Напиши: "Привет"')
@@ -22,21 +22,22 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
 
 
+@bot.message_handler(func=lambda m: True)
 def give_affirmation(message):
     if message.text == 'аффирмация' or message.text == 'Аффирмация':
         keyboard = types.InlineKeyboardMarkup()
         key_affirmation = types.InlineKeyboardButton(text='Получить позитивную аффирмацию', callback_data='get_affirm')
         keyboard.add(key_affirmation)
-
+        bot.send_message(message.from_user.id, text='Чтобы получить позитивную аффирмацию, нажми на кнопку: ',
+                         reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == 'get_affirm':
+        bot.send_message(call.message.chat.id, random.choice(affirmations))
 
-        bot.send_message(call.message.chat.id, 'Запомню : )');
 
-
-bot.polling()
+bot.infinity_polling()
 
 
